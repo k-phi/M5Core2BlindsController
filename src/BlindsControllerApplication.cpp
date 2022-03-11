d4b13f811fbe6e3939a223e2201daca72e6755e7
k-phi <kraft.philipp@gmail.com>
2022-03-11 23:53:28 +0100

    Comment unnecessary stuff for now

--- src/BlindsControllerApplication.cpp
+++ src/BlindsControllerApplication.cpp
@@ -1,12 +1,12 @@
 #include "BlindsControllerApplication.h"
-#include "ITestDummy.h"
-#include "TestDummyM5Core2.h"
+// #include "ITestDummy.h"
+// #include "TestDummyM5Core2.h"
 
 void BlindsControllerApplication::setup()
 {
-    ITestDummy *dummy = new TestDummyM5Core2();
-    dummy->showHellowWorld();
-    dummy->~ITestDummy();
+    // ITestDummy *dummy = new TestDummyM5Core2();
+    // dummy->showHellowWorld();
+    // dummy->~ITestDummy();
 }
 
 void BlindsControllerApplication::loop()
