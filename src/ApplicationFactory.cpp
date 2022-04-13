c28c317a02402e098b5d5fe0c147577f0f32a80f
k-phi <kraft.philipp@gmail.com>
2022-04-13 21:34:19 +0200

    Completet factory implementation

--- src/ApplicationFactory.cpp
+++ src/ApplicationFactory.cpp
@@ -1,13 +1,53 @@
 #include "ApplicationFactory.h"
 
+#include "backend/BlindsAccess.h"
+#include "backend/BlindsManager.h"
+#include "backend/DeviceAccess.h"
+#include "backend/DeviceDataAccess.h"
+#include "backend/DeviceManager.h"
+#include "backend/HttpBlind.h"
+#include "firmwareAbstraction/Device.h"
+#include "firmwareAbstraction/DeviceData.h"
 #include "firmwareAbstraction/HardwareButton.h"
+#include "firmwareAbstraction/HttpClientWrapper.h"
 #include "firmwareAbstraction/SelectionButton.h"
 #include "firmwareAbstraction/StatusBar.h"
 #include "firmwareAbstraction/TouchButton.h"
 
-ApplicationFactory::~ApplicationFactory()
-{
-    
+ApplicationFactory::~ApplicationFactory() {
+    for (unsigned int configIndex = 0; configIndex < numberOfConfigs_;
+         configIndex++) {
+        delete *(selectionButtons_ + configIndex);
+        delete *(blinds_ + configIndex);
+        delete *(loopers_ + configIndex);
+    }
+
+    delete blindIds_;
+    delete application_;
+    delete blindsControlViewController_;
+    delete blindsControlView_;
+    delete powerOffButton_;
+    delete toggleAllSelectionButton_;
+    delete statusBar_;
+    delete selectionButtons_;
+    delete openButton_;
+    delete closeButton_;
+    delete tiltButton_;
+    delete stopButton_;
+    delete device_;
+    delete deviceData_;
+    delete httpClient_;
+    delete blinds_;
+    delete loopers_;
+    delete deviceAccess_;
+    delete deviceDataAccessPersistable_;
+    delete deviceDataAccessEnablable_;
+    delete blindsAccess_;
+    delete blindsAccessLooper_;
+    delete deviceManager_;
+    delete blindsManagerBlind_;
+    delete blindsManagerEnablable_;
+    delete blindsManagerLooper_;
 }
 
 Application *ApplicationFactory::createApplication(
@@ -16,9 +56,86 @@ Application *ApplicationFactory::createApplication(
     applicationConfiguration_ = applicationConfiugration;
     blindConfigurations_ = blindConfigurations;
     numberOfConfigs_ = numberOfConfigs;
+    blindIds_ = new unsigned int[numberOfConfigs_];
+    for (unsigned int configIndex = 0; configIndex < numberOfConfigs_;
+         configIndex++) {
+        blindIds_[configIndex] = blindConfigurations_[configIndex].id;
+    }
 
+    createDevice();
+    createDeviceData();
+    createHttpClient();
+    createBlinds();
+    createDeviceAccess();
+    createDeviceDataAccess();
+    createBlindsAccess();
+    createDeviceManager();
+    createBlindsManager();
     createBlindsControlView();
-    return nullptr;
+    createBlindsControlViewController();
+    application_ = new Application(blindsControlViewController_);
+    return application_;
+}
+
+void ApplicationFactory::createDevice() {
+    unsigned int connectionTimeoutInMilliseconds = 200;
+    device_ = new Device(applicationConfiguration_->ssid,
+                         applicationConfiguration_->passphrase,
+                         connectionTimeoutInMilliseconds);
+}
+
+void ApplicationFactory::createDeviceData() {
+    char storageName[50] = "M5Core2BlindsController";
+    deviceData_ = new DeviceData(storageName);
+}
+
+void ApplicationFactory::createHttpClient() {
+    httpClient_ = new HttpClientWrapper();
+}
+
+void ApplicationFactory::createBlinds() {
+    blinds_ = new IBlind *[numberOfConfigs_];
+    for (unsigned int configIndex = 0; configIndex < numberOfConfigs_;
+         configIndex++) {
+        HttpBlind *blind =
+            new HttpBlind(*(blindConfigurations_ + configIndex), httpClient_,
+                          applicationConfiguration_
+                              ->timeoutBetweenHttpRequestsInMilliseconds);
+        *(blinds_ + configIndex) = blind;
+
+        *(loopers_ + configIndex) = blind;
+    }
+}
+
+void ApplicationFactory::createDeviceAccess() {
+    deviceAccess_ = new DeviceAccess(device_);
+}
+
+void ApplicationFactory::createDeviceDataAccess() {
+    DeviceDataAccess *access = new DeviceDataAccess(deviceData_);
+    deviceDataAccessPersistable_ = access;
+    deviceDataAccessEnablable_ = access;
+}
+
+void ApplicationFactory::createBlindsAccess() {
+    BlindsAccess *access =
+        new BlindsAccess(blinds_, loopers_, numberOfConfigs_);
+    blindsAccess_ = access;
+    blindsAccessLooper_ = access;
+}
+
+void ApplicationFactory::createDeviceManager() {
+    deviceManager_ =
+        new DeviceManager(deviceAccess_, deviceDataAccessPersistable_);
+}
+
+void ApplicationFactory::createBlindsManager() {
+    BlindsManager *blindsManager = new BlindsManager(
+        blindIds_, numberOfConfigs_, deviceDataAccessEnablable_, blindsAccess_,
+        blindsAccessLooper_);
+    blindsManagerBlind_ = blindsManager;
+    blindsManagerEnablable_ = blindsManager;
+    blindsManagerLooper_ = blindsManager;
 }
 
 void ApplicationFactory::createBlindsControlView() {
@@ -48,12 +165,18 @@ void ApplicationFactory::createBlindsControlView() {
         closeButton_);
 }
 
+void ApplicationFactory::createBlindsControlViewController() {
+    blindsControlViewController_ = new BlindsControlViewController(
+        blindIds_, numberOfConfigs_, blindsControlView_, deviceManager_,
+        blindsManagerEnablable_, blindsManagerBlind_, blindsManagerLooper_,
+        applicationConfiguration_->idleTimeoutBeforeShutdownInMilliseconds);
+}
+
 ISelectionButton *ApplicationFactory::createSelectionButton(
     BlindConfiguration &blindconfiguration) {
     SelectionButton *button = new SelectionButton(
         blindconfiguration.buttonPositionX, blindconfiguration.buttonPositionY,
-        blindconfiguration.buttonWidth, blindconfiguration.buttonHeight, blindconfiguration.id);
+        blindconfiguration.buttonWidth, blindconfiguration.buttonHeight,
+        blindconfiguration.id);
     return button;
 }
-
-
--- src/ApplicationFactory.h
+++ src/ApplicationFactory.h
@@ -1,52 +1,75 @@
 #ifndef __APPLICATIONFACTORY_H__
 #define __APPLICATIONFACTORY_H__
 
+#include "Application.h"
 #include "ApplicationConfiguration.h"
 #include "BlindConfiguration.h"
-#include "Application.h"
+#include "backend/IBlind.h"
+#include "backend/IBlinds.h"
+#include "backend/IDevice.h"
+#include "backend/IDeviceData.h"
+#include "backend/IEnablable.h"
+#include "backend/IHttpClient.h"
+#include "backend/ILooper.h"
+#include "backend/IPersistable.h"
 #include "hmi/BlindsControlView.h"
 #include "hmi/BlindsControlViewController.h"
 #include "hmi/ISelectionButton.h"
 #include "hmi/IStatusBar.h"
 #include "hmi/ITouchButton.h"
-#include "backend/BlindsAccess.h"
-#include "backend/BlindsManager.h"
-#include "backend/DeviceAccess.h"
-#include "backend/DeviceDataAccess.h"
-#include "backend/DeviceManager.h"
-#include "backend/HttpBlind.h"
-#include "backend/IDevice.h"
-#include "backend/IDeviceData.h"
-#include "backend/IHttpClient.h"
-
 
 class ApplicationFactory {
    public:
     ~ApplicationFactory();
-    Application* createApplication(
-        ApplicationConfiguration* applicationConfiugration,
-        BlindConfiguration* blindConfigurations, unsigned int numberOfConfigs);
+    Application *createApplication(
+        ApplicationConfiguration *applicationConfiugration,
+        BlindConfiguration *blindConfigurations, unsigned int numberOfConfigs);
 
    private:
-   ApplicationConfiguration *applicationConfiguration_;
-   unsigned int numberOfConfigs_;
-   BlindConfiguration *blindConfigurations_;
-   Application* application_;
-   BlindsControlViewController *blindsControlViewController_;
-   BlindsControlView *blindsControlView_;
-   ITouchButton *powerOffButton_;
-   ITouchButton *toggleAllSelectionButton_;
-   IStatusBar *statusBar_;
-   ISelectionButton **selectionButtons_;
-   ITouchButton *openButton_;
-   ITouchButton *closeButton_;
-   ITouchButton *tiltButton_;
-   ITouchButton *stopButton_;
+    ApplicationConfiguration *applicationConfiguration_;
+    BlindConfiguration *blindConfigurations_;
+    unsigned int numberOfConfigs_;
 
+    unsigned int *blindIds_;
+    Application *application_;
+    BlindsControlViewController *blindsControlViewController_;
+    BlindsControlView *blindsControlView_;
+    ITouchButton *powerOffButton_;
+    ITouchButton *toggleAllSelectionButton_;
+    IStatusBar *statusBar_;
+    ISelectionButton **selectionButtons_;
+    ITouchButton *openButton_;
+    ITouchButton *closeButton_;
+    ITouchButton *tiltButton_;
+    ITouchButton *stopButton_;
+    IDevice *device_;
+    IDeviceData *deviceData_;
+    IHttpClient *httpClient_;
+    IBlind **blinds_;
+    ILooper **loopers_;
+    IDevice *deviceAccess_;
+    IPersistable *deviceDataAccessPersistable_;
+    IEnablable *deviceDataAccessEnablable_;
+    IBlinds *blindsAccess_;
+    ILooper *blindsAccessLooper_;
+    IDevice *deviceManager_;
+    IBlind *blindsManagerBlind_;
+    IEnablable *blindsManagerEnablable_;
+    ILooper *blindsManagerLooper_;
 
+    void createDevice();
+    void createDeviceData();
+    void createHttpClient();
+    void createBlinds();
+    void createDeviceAccess();
+    void createDeviceDataAccess();
+    void createBlindsAccess();
+    void createDeviceManager();
+    void createBlindsManager();
     void createBlindsControlView();
-    ISelectionButton* createSelectionButton(
-        BlindConfiguration& blindconfiguration);
+    void createBlindsControlViewController();
+    ISelectionButton *createSelectionButton(
+        BlindConfiguration &blindconfiguration);
 };
 
 #endif  // __APPLICATIONFACTORY_H__
